658c5c0a66dbf9da88fa9e688cbc125f361e2694
Kouadio Kenneth <34052222+kennethKing6@users.noreply.github.com>
2020-12-24 20:14:50 -0800

    edit the codebase to follow the eslint suggestions

--- src/BLEUtils/BLESupport.js
+++ src/BLEUtils/BLESupport.js
@@ -1,79 +1,69 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/prefer-default-export */
 import { BleManager } from 'react-native-ble-plx';
 
-
 export class BLESupport {
-
-    constructor(){
-        /**
+  constructor() {
+    /**
          * manager enables us to have access to the react native
          * BLE api
          */
-        this.manager = new BleManager();
-        /**
+    this.manager = new BleManager();
+    /**
          * Device represents a BLE device.
          * undefined means we have not yet looked for a device and there is no device yet
          * null means we have tried looking for a device and something caused it to fail
          * or It has a device that we can connect to and work with
          */
-        this.device = undefined;
+    this.device = undefined;
+  }
 
-    }
-    
-    /**
+  /**
      * This method is used to find a device
-     * ble device it finds. 
+     * ble device it finds.
      */
-    findDevice(){
-         
-         return new Promise((resolve,reject)=>{
+  findDevice() {
+    return new Promise((resolve, reject) =>
+      // eslint-disable-next-line implicit-arrow-linebreak
+      this.manager.startDeviceScan(null, null, (error, device) => {
+        if (error) {
+          this.device = null;
+          reject(error);
+        }
 
-             return this.manager.startDeviceScan(null, null, (error, device) => {
-                if (error) {
-                    this.device = null;
-                    reject(error)                    
-                }
-        
-                
-                // Check if it is a device you are looking for based on advertisement data
-                // or other criteria.
-                if (device) {
-                    this.device = device;
-                    this.manager.stopDeviceScan();
-                    resolve(device);
-                
-                }
-                })
-         })  
-    }
+        // Check if it is a device you are looking for based on advertisement data
+        // or other criteria.
+        if (device) {
+          this.device = device;
+          this.manager.stopDeviceScan();
+          resolve(device);
+        }
+      }));
+  }
 
-    
-    getBLEManager(){
-        return this.manager;
-    }
+  getBLEManager() { return this.manager; }
 
-    /**
+  /**
      * Wait until you at least get a device or you tried at least once
      */
-    getDevice(){
-        return new Promise((resolve,reject)=>{
-            //Make sure to wait until you either get a device or no device
-            const timer = setInterval(()=>{
-                if(this.device === null){
-                    clearInterval(timer);
-                    reject(null);
-                    
-                }
-            else{
-                resolve(this.device);
-                clearInterval(timer);
+  getDevice() {
+    return new Promise((resolve, reject) => {
+      // Make sure to wait until you either get a device or no device
+      const timer = setInterval(() => {
+        if (this.device === null) {
+          clearInterval(timer);
+          // eslint-disable-next-line prefer-promise-reject-errors
+          reject(null);
+        } else {
+          resolve(this.device);
+          clearInterval(timer);
+        }
+      },
+      100);
+    });
+  }
 
-            }
-                
-            },100);
-        })
-    }
-
-    hasDevice(){
-       return this.device !== undefined && this.device !== null;
-    }
+  hasDevice() {
+    return this.device !== undefined && this.device !== null;
+  }
 }
--- src/GraphUtils/GraphData.js
+++ src/GraphUtils/GraphData.js
@@ -1,61 +1,63 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+
 /**
  * This class is used to model the data of the graph.
- * 
- * The graph takes an array of objects with each object containing 
+ * The graph takes an array of objects with each object containing
  * containing an X-coordinate value and a Y-coordinate value
- * 
  */
-export class GraphData{
-
-    constructor(){
+export class GraphData {
+    constructor() {
         /**
          * This array contains the list of data objects
          * to be plotted on the graph
          */
-        
+
         this.graphData = [];
     }
+
     /**
      * This method is used to determine the origin of the graph
      */
-    getGraphOrigin(){
-        if(this.graphData.length > 1)
-            return this.graphData[parseInt(this.graphData.length - 1)];
-        else
-            return {x:0,y:0};
+    getGraphOrigin() {
+        // eslint-disable-next-line max-len
+        if (this.graphData.length > 1) return this.graphData[parseInt(this.graphData.length - 1, 10)];
+        return { x: 0, y: 0 };
     }
-    
+
     /**
      * This method main purpose is to store the bletooth device single reading
      * to an array of graphdata. Remember a graph data contains an X and Y value
-     * 
-     * @param {objectData} objectData is a sigle x and y value to be plotted 
-     *    
+     *
+     * @param {objectData} objectData is a sigle x and y value to be plotted
+     *
      */
-      addGraphData(objectData){
+      addGraphData(objectData) {
             this.graphData.push(objectData);
       }
+
       /**
        * This returns an array of all objects containing x and y
        * values to be plotted
-       * 
-       * @return {graphData} 
+       *
+       * @return {graphData}
        */
-      getData(){
+      getData() {
         return this.graphData;
       }
+
       /**
        * This method is used to initialize the graph. it is very essential
        * as it is needed to for the graph to have an object of X and Y value
        * eventhough we cannot yet read data from the ble bluetooth device
        */
-       dataInitializer(){
-        //This data is used to initialize the chart when there is no signal yet or no readings
-      const dataInitializer = [{x:0,y:0}];
+       dataInitializer() {
+        // This data is used to initialize the chart when there is no signal yet or no readings
+      const dataInitializer = [{ x: 0, y: 0 }];
 
-        if(this.graphData.length === 0)
-          return dataInitializer;
-        else
-          return this.graphData;
+        if (this.graphData.length === 0) return dataInitializer;
+        return this.graphData;
       }
 }
